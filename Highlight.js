6a354cf85a13433ae538d7a107724a940dd7f1c2
Martin Henry <emailnitram@gmail.com>
2016-10-17 17:31:32 -0700

    update to ios app

--- Highlight.js
+++ Highlight.js
@@ -5,13 +5,15 @@ import {
   Text,
   View,
   Image,
+  ActivityIndicator,
   ActionSheetIOS,
   TouchableHighlight
 } from 'react-native';
 
 import VideoPlayer from 'react-native-video-player';
+import timeAgo from 'time-ago';
 
-const VIMEO_ID = '179859217';
+let ta = timeAgo();
 
 export default class Highlight extends Component {
 
@@ -22,19 +24,14 @@ export default class Highlight extends Component {
       video: { width: undefined, height: undefined, duration: undefined },
       thumbnailUrl: undefined,
       videoUrl: undefined,
-      title: undefined,
-      shortCode: undefined
+      error: false
     };
   }
 
   componentDidMount() {
-    // console.log('this', this.props.video)
-    // let shortcode = this.props.video
     global.fetch(`https://api.streamable.com/videos/${this.props.shortCode}`)
       .then(res => res.json())
-      // .then(res => console.log('res',res.thumbnail_url))
       .then(res => this.setState({
-        shortCode: this.props.shortCode,
         thumbnailUrl: `https:${res.thumbnail_url}`,
         videoUrl: `https:${res.files['mp4-mobile'].url}`,
         video: { 
@@ -42,17 +39,14 @@ export default class Highlight extends Component {
           height: res.files['mp4-mobile'].height,
           duration: res.files['mp4-mobile'].duration
         }
-      }));
+      }))
+      .catch(error => this.setState({error: true}) );
   }
 
   showShareActionSheet(shortCode) {
     ActionSheetIOS.showShareActionSheetWithOptions({
       url: `https://streamable.com/${shortCode}`,
-      message: this.props.title,
-      subject: 'a subject to go in the email heading',
-      excludedActivityTypes: [
-        'com.apple.UIKit.activity.PostToTwitter'
-      ]
+      message: this.props.title
     },
     (error) => alert(error),
     (success, method) => {
@@ -62,41 +56,98 @@ export default class Highlight extends Component {
       } else {
         text = 'You didn\'t share';
       }
-      this.setState({text});
+      // this.setState({text});
     });
   };
 
   render() {
+
+    if(this.state.videoUrl === undefined) {
+      return (
+        <View style={styles.centering}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
+
+    if(this.state.error) {
+      return (
+        <View>
+          <Text style={styles.title}>Error loading Video</Text>
+        </View>
+      )
+    }
     return (
       <View style={styles.view}>
-        <Text style={styles.title}>{this.props.title}</Text>
-        <TouchableHighlight onPress={() => this.showShareActionSheet(this.state.shortCode)}>
-          <Image source={require('./share.png')} style={{width: 19, height: 27}} />
-        </TouchableHighlight>
-        <VideoPlayer
-          thumbnail={{ uri: this.state.thumbnailUrl }}
-          video={{ uri: this.state.videoUrl }}
-          videoWidth={this.state.video.width}
-          videoHeight={this.state.video.height}
-          duration={this.state.video.duration/* I'm using a hls stream here, react-native-video
-            can't figure out the length, so I pass it here from the vimeo config */}
-        />
+        <View style={styles.timeContainer}>
+          <Text style={styles.time}>{ta.ago(new Date(this.props.created_utc * 1000))}</Text>
+        </View>
+        <View style={styles.video}>
+          <VideoPlayer
+            thumbnail={{ uri: this.state.thumbnailUrl }}
+            video={{ uri: this.state.videoUrl }}
+            videoWidth={this.state.video.width}
+            videoHeight={this.state.video.height}
+            duration={this.state.video.duration/* I'm using a hls stream here, react-native-video
+              can't figure out the length, so I pass it here from the vimeo config */}
+          />
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.title}>{this.props.title}</Text>
+          <TouchableHighlight onPress={() => this.showShareActionSheet(this.props.shortCode)} style={styles.share} underlayColor={'transparent'}>
+            <Image source={require('./share-arrow.png')} style={{width: 25, height: 25}} />
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 10,
+    flexDirection: 'row',
+  },
   button: {
     marginBottom: 10,
     fontWeight: '500',
   },
+  timeContainer: {
+    flexDirection: 'row-reverse',
+  },
+  video: {
+    borderColor: 'black',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    backgroundColor: 'black'
+  },
+  time: {
+    fontSize: 10,
+    marginRight: 5,
+    marginBottom: 2,
+    color: '#444444'
+  },
   title: {
-    paddingLeft: 5,
-    paddingRight: 5,
-    fontSize: 18
+    flex: 8,
+    fontSize: 14,
+    paddingTop: 5,
+    paddingRight: 5
+  },
+  share: {
+    flex: 1,
+    alignSelf: 'center',
+    paddingTop: 5
   },
   view: {
-    marginBottom: 20
+    paddingTop: 20
+  },
+  centering: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300
   }
 });
--- Highlights.js
+++ Highlights.js
@@ -3,10 +3,14 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View,
+  TouchableHighlight,
   ActivityIndicator,
   StatusBar,
-  ListView
+  ListView,
+  RefreshControl,
+  Platform
 } from 'react-native';
 
 import VideoPlayer from 'react-native-video-player';
@@ -23,6 +27,8 @@ export default class Highlights extends Component {
       after: '',
       videoList: [],
       dataSource: ds.cloneWithRows([]),
+      error: false,
+      refreshing: false
     };
   }
 
@@ -30,47 +36,78 @@ export default class Highlights extends Component {
     this.callRedditApi();
   }
 
+  _onRefresh() {
+    this.setState({refreshing: true, videoList: [], dataSource: ds.cloneWithRows([])});
+    this.callRedditApi();
+  }
+
+  _retryLoad() {
+    this.callRedditApi();
+  }
+
   callRedditApi(after = '') {
     fetch(`https://www.reddit.com/r/nba.json?after=${after}&raw_json=1`).then(resp => resp.json()).then(data => {
       let videos = data.data.children.filter(item => {
         let url = item.data.url;
-        // return url.match(/streamable/) || url.match(/youtube.com\/watch/) || url.match(/youtu.be/)
         return url.match(/streamable/)
       })
 
       let vids = videos.map((v) => {
-        console.log('v', v)
-        return { shortCode: v.data.url.split('/')[3], title: v.data.title }
+        return { shortCode: v.data.url.split('/')[3], title: v.data.title, created_utc: v.data.created_utc }
       })
 
-      console.log('results', vids)
+      vids = vids.filter((v) => {
+        return v.shortCode.length === 4;
+      })
+
       this.setState({
         after: data.data.after,
         videoList: this.state.videoList.concat(vids),
-        dataSource: ds.cloneWithRows(this.state.videoList.concat(vids))
+        dataSource: ds.cloneWithRows(this.state.videoList.concat(vids)),
+        refreshing: false,
+        error: false
       })
-      if(this.state.videoList.length < 4) {
+      if(this.state.videoList.length < 12) {
         this.callRedditApi(this.state.after);
       }
-      // })
     })
+    .catch(error => this.setState({error: true}));
   }
 
   render() {
+    if(this.state.error) {
+      return (
+        <View style={styles.centering}>
+          <TouchableHighlight onPress={() => this._retryLoad()} underlayColor={'transparent'}>
+            <Image source={require('./warning.png')} style={{width: 150, height: 150}} />
+          </TouchableHighlight>
+          <Text style={styles.errorText}>An error has occured</Text>
+          <Text style={styles.errorText}>Check internet connection</Text>
+          <Text style={styles.errorText}>Press triangle to try again</Text>
+          <Text style={[styles.errorText, styles.emojiText]}>😥</Text>
+        </View>
+      )
+    }
     if(this.state.videoList.length === 0) {
       return (
-        <ActivityIndicator
-          style={[styles.centering, {transform: [{scale: 1.5}]}]}
-          size="large"
-        />
+        <View style={[styles.centering, {transform: [{scale: 1.5}]}]}>
+          <ActivityIndicator />
+          <Text style={styles.loading}>LOADING</Text>
+        </View>
       )
     }
     return (
       <ListView
         style={styles.container}
         enableEmptySections={true}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh.bind(this)}
+          />
+        }
         dataSource={this.state.dataSource}
-        renderRow={(rowData) => <Highlight shortCode={rowData.shortCode} title={rowData.title}></Highlight>}
+        renderRow={(rowData) => <Highlight shortCode={rowData.shortCode} title={rowData.title} created_utc={rowData.created_utc}></Highlight>}
       />
     );
   }
@@ -82,8 +119,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  loading: {
+    fontSize: 8
+  },
+  errorText: {
+    fontSize: 14
+  },
+  emojiText: {
+    paddingBottom: 5
+  },
   container: {
     flex: 1,
-    marginTop: 20,
+    marginTop: (Platform.OS === 'ios') ? 20 : 0,
   }
 });
